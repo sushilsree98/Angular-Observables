24d860c9993039092da559710d68f489cbf46a97
sushilsree98 <sushil.sree98@gmail.com>
2020-05-24 19:52:19 +0530

    Custom Observables added

--- src/app/page-one/page-one.component.ts
+++ src/app/page-one/page-one.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-page-one',
@@ -7,14 +7,26 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./page-one.component.css']
 })
 export class PageOneComponent implements OnInit, OnDestroy {
-
+  customObservable;
   private firstObsListener: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    this.firstObsListener = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObsListener = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // })
+
+    /* Creating a custom Observable */
+    this.customObservable = Observable.create(observer=>{
+      let count = 0
+      setInterval(()=>{
+        observer.next(count)
+        count++;
+      },1000)
+    });
+    this.firstObsListener = this.customObservable.subscribe(data=>{
+      console.log(data);
     })
   }
 
